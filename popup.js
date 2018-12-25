e1103c0fdaf4dd3ea98802a2a6a9197ca30fc7db
Zack Holmberg <zack_holmberg@outlook.com>
2018-12-25 00:23:32 -0600

    Added the basics.
    Added the necessary files, such as an html file for the popup, a css file for the styling, a javascript file to display the html popup, and a manifest file. Wrote basic code for each file to display a static image and basic text.  Implementation of pulling a random meme from the web is next.

---
+++ popup.js
@@ -0,0 +1,15 @@
+  function clickHandler(e) {
+    //setTimeout(func1, 1000);
+  }
+  
+  function main() {
+    // Initialization work goes here.
+  }
+  
+  // Add event listeners once the DOM has fully loaded by listening for the
+  // `DOMContentLoaded` event on the document, and adding your listeners to
+  // specific elements when it triggers.
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', clickHandler);
+    main();
+  });
