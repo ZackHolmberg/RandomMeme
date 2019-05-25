4cd98021c340a97151c78e876e05e4d4d7d0f596
Zack Holmberg <zack_holmberg@outlook.com>
2019-05-25 18:19:55 -0500

    Cleanup and added a few pictures for first release.

--- popup.js
+++ popup.js
@@ -5,48 +5,53 @@ $.ajaxSetup({
 var array = [];
 
 window.onload = function () {
-  document.getElementById("text").innerHTML = "This should change"
   getMemes();
-  function getMemes() {
-    $.getJSON("http://www.reddit.com/r/dankmemes/.json", function (data) {
-      $.each(data.data.children, function (i, item, temp) {
-        if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png") || item.data.url.includes(".gif")))
-          array.push(item.data.url);
-      });
-
-    });
-
-    $.getJSON("http://www.reddit.com/r/funny/.json", function (data) {
-      $.each(data.data.children, function (i, item, temp) {
-        if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png") || item.data.url.includes(".gif")))
-          array.push(item.data.url);
-      });
-    });
-
-    $.getJSON("http://www.reddit.com/r/memes/.json", function (data) {
-      $.each(data.data.children, function (i, item, temp) {
-        if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png") || item.data.url.includes(".gif")))
-          array.push(item.data.url);
-      });
-    });
-
-    $.getJSON("http://www.reddit.com/r/dank_meme/.json", function (data) {
-      $.each(data.data.children, function (i, item, temp) {
-        if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png") || item.data.url.includes(".gif")))
-          array.push(item.data.url);
-      });
-    });
-
-    getMeme();
-  }
+  getMeme();
 }
 
 
 
 function getMeme() {
   var index = Math.floor(Math.random() * array.length);
-  $("<img/>").attr("src", array[index]).appendTo("#images");
+  $("#my_image").attr("src", array[index]);
   array.pop(array[index]);
-  document.getElementById("text").innerHTML = "Image URL: " + array[index] + "<br>" +
-    "Number of memes left in array: " + array.length;
 }
+
+function getMemes() {
+  $.getJSON("http://www.reddit.com/r/dankmemes/.json", function (data) {
+    $.each(data.data.children, function (i, item, temp) {
+      if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png")))
+        array.push(item.data.url);
+    });
+
+  });
+
+  $.getJSON("http://www.reddit.com/r/funny/.json", function (data) {
+    $.each(data.data.children, function (i, item, temp) {
+      if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png")))
+        array.push(item.data.url);
+    });
+  });
+
+  $.getJSON("http://www.reddit.com/r/memes/.json", function (data) {
+    $.each(data.data.children, function (i, item, temp) {
+      if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png")))
+        array.push(item.data.url);
+    });
+  });
+
+  $.getJSON("http://www.reddit.com/r/dank_meme/.json", function (data) {
+    $.each(data.data.children, function (i, item, temp) {
+      if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png")))
+        array.push(item.data.url);
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('myButton');
+  // onClick's logic below:
+  link.addEventListener('click', function() {
+      getMeme();
+  });
+});
