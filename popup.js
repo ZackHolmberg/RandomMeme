4cc2f108e2a946ec5cf46e487fe3059afd9fd3f9
Zack Holmberg <zack_holmberg@outlook.com>
2019-05-25 16:28:08 -0500

    Connected to Reddit API.
    Wrote code to connect to reddit API and pull top memes from different subreddits and put them in an array, then randomnly select on to display.

--- popup.js
+++ popup.js
@@ -1,15 +1,52 @@
-  function clickHandler(e) {
-    //setTimeout(func1, 1000);
+$.ajaxSetup({
+  async: false
+});
+
+var array = [];
+
+window.onload = function () {
+  document.getElementById("text").innerHTML = "This should change"
+  getMemes();
+  function getMemes() {
+    $.getJSON("http://www.reddit.com/r/dankmemes/.json", function (data) {
+      $.each(data.data.children, function (i, item, temp) {
+        if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png") || item.data.url.includes(".gif")))
+          array.push(item.data.url);
+      });
+
+    });
+
+    $.getJSON("http://www.reddit.com/r/funny/.json", function (data) {
+      $.each(data.data.children, function (i, item, temp) {
+        if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png") || item.data.url.includes(".gif")))
+          array.push(item.data.url);
+      });
+    });
+
+    $.getJSON("http://www.reddit.com/r/memes/.json", function (data) {
+      $.each(data.data.children, function (i, item, temp) {
+        if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png") || item.data.url.includes(".gif")))
+          array.push(item.data.url);
+      });
+    });
+
+    $.getJSON("http://www.reddit.com/r/dank_meme/.json", function (data) {
+      $.each(data.data.children, function (i, item, temp) {
+        if (i != 0 && (item.data.url.includes(".jpg") || item.data.url.includes(".jpeg") || item.data.url.includes(".png") || item.data.url.includes(".gif")))
+          array.push(item.data.url);
+      });
+    });
+
+    getMeme();
   }
-  
-  function main() {
-    // Initialization work goes here.
-  }
-  
-  // Add event listeners once the DOM has fully loaded by listening for the
-  // `DOMContentLoaded` event on the document, and adding your listeners to
-  // specific elements when it triggers.
-  document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('button').addEventListener('click', clickHandler);
-    main();
-  });
+}
+
+
+
+function getMeme() {
+  var index = Math.floor(Math.random() * array.length);
+  $("<img/>").attr("src", array[index]).appendTo("#images");
+  array.pop(array[index]);
+  document.getElementById("text").innerHTML = "Image URL: " + array[index] + "<br>" +
+    "Number of memes left in array: " + array.length;
+}
